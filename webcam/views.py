38d0735440e50d14cb472b7e97b16f6a22663aee
Hoang <hoangnt2601@gmail.com>
2019-08-28 10:11:05 +0700

    fixed import lib

--- webcam/views.py
+++ webcam/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, StreamingHttpResponse
 from django.template import loader
+import cv2
 
 # Create your views here.
 def index(request):

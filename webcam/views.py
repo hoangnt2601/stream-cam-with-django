403357c667b1e1351857a65afa40859b137ee485
Hoang <hoangnt2601@gmail.com>
2019-08-28 09:45:49 +0700

    init

---
+++ webcam/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+def index(request):
+    template = loader.get_template('webcam/index.html')
+    return HttpResponse(template.render({}, request))
+
+def stream():
+    cap = cv2.VideoCapture(0) 
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            print("Error: failed to capture image")
+            break
+
+        cv2.imwrite('demo.jpg', frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + open('demo.jpg', 'rb').read() + b'\r\n')
+
+def video_feed(request):
+    return StreamingHttpResponse(stream(), content_type='multipart/x-mixed-replace; boundary=frame')
